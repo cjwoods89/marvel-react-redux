ea7afd76c4308b18b1b0917233999b35aa815db3
Christopher Woods <Christopher.Woods@LibertyMutual.com>
2017-07-12 15:41:53 -0400

    Fix API Key

--- src/actions/characters.js
+++ src/actions/characters.js
@@ -4,8 +4,8 @@ import md5 from 'md5'
 const baseUrl = 'http://gateway.marvel.com/v1/public/';
 
 function generateCredentials(){
-  let publicKey = process.env.REACT_APP_MARVEL_PUBLIC_KEY;
-  let privateKey = process.env.REACT_APP_MARVEL_PRIVATE_KEY;
+  var publicKey = process.env.REACT_APP_MARVEL_PUBLIC_KEY;
+  var privateKey = process.env.REACT_APP_MARVEL_PRIVATE_KEY;
   let timestamp = +new Date(); // same new Date().getTime()
   let hash = md5(`${timestamp}${privateKey}${publicKey}`);
   return `?ts=${timestamp}&apikey=${publicKey}&hash=${hash}`
